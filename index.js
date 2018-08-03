cf4200ed6c2f99eefd5f5f65e9991f9293d0c4c6
Kaelinator <kirkkael@gmail.com>
2018-08-03 00:08:11 -0500

    trying an rnn

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const tf = require('@tensorflow/tfjs')
 require('@tensorflow/tfjs-node')
 
-const model = tf.sequential()
-model.add(tf.layers.dense({
-  units: 100,
-  activation: 'relu',
-  inputShape: [10]
-}))
-model.add(tf.layers.dense({
-  units: 1,
-  activation: 'linear'
-}))
+const cells = [
+  tf.layers.lstmCell({units: 4}),
+  tf.layers.lstmCell({units: 8})
+]
+
+const model = tf.sequential({ 
+  layers: [
+    tf.layers.dense({ units: 1, activation: 'sigmoid', inputShape: [2] }),
+    tf.layers.rnn({cell: cells, returnSequences: true }),
+    // tf.layers.dense({ units: 1, activation: 'sigmoid' }),
+    tf.layers.dense({ units: 1, activation: 'sigmoid' })
+    // tf.layers.lstm({ units: 100, activation: 'sigmoid', returnSequences: true }),
+  ]
+})
+
 model.compile({
   optimizer: 'sgd',
   loss: 'meanSquaredError'
 })
 
-const xs = tf.randomNormal([100, 10])
-const ys = tf.randomNormal([100, 1])
+// const xs = tf.randomNormal([100, 2])
+// const ys = tf.randomNormal([100, 1])
 
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: async (epoch, log) => {
-      console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-    }
-  }
-})
+// xs.print()
+// ys.print()
+
+// model.fit(xs, ys, {
+//   epochs: 100,
+//   callbacks: {
+//     onEpochEnd: (epoch, log) => {
+//       console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+//     },
+//     onTrainEnd: (x, y, z) => {
+//       console.log('x', x, 'y', y, 'z', z)
+//     }
+//   }
+// })
