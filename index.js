a6b78eca9d35a3225924367adaa1dda9bf84bf31
Kaelinator <kirkkael@gmail.com>
2018-08-04 01:53:47 -0500

    better layers and saving

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const tf = require('@tensorflow/tfjs')
 require('@tensorflow/tfjs-node')
 
-const rnn = tf.layers.rnn({
-  cell: [
-    tf.layers.lstmCell({units: 50})
-  ],
-  returnSequences: true,
-  inputShape: [10, 1] // [timesteps, feature]
-})
-
 const model = tf.sequential({
   layers: [
-    rnn,
-    tf.layers.dense({ units: 10 })
+    tf.layers.dense({ units: 16, activation: 'linear', inputShape: [10, 1] }),
+    tf.layers.lstm({ units: 128, returnSequences: true, recurrentActivation: 'softplus' }),
+    tf.layers.dense({ units: 10, activation: 'sigmoid' })
   ]
 })
 
@@ -23,30 +16,38 @@ model.compile({
 })
 
 /* [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, ... ] */
-const data = Int32Array.from(Array(1000)
+const data = Int32Array.from(Array(100)
   .fill(0)
   .reduce((arr, x, i) => arr.concat((i).toString().split('')), []), x => +x)
 
-const ddata = Array.from(data.slice(1)).reduce((a, x) => { 
-  const arr = Array(10).fill(0)
-  arr[x] = 1
-  return a.concat(arr)
-}, [])
+const batchSize = data.length / 10
 
-const xs = tf.tensor1d(data).reshape([289, 10, 1])
-const ys = tf.tensor1d([...ddata, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).reshape([289, 10, 10])
+const xs = tf.tensor1d(data, 'int32').reshape([batchSize, 10, 1])
+const ys = tf.oneHot(tf.tensor1d([...data.slice(1), 0], 'int32'), 10).reshape([batchSize, 10, 10]).toFloat()
 
 model.fit(xs, ys, {
-  epochs: 1,
-  batchSize: 289,
+  epochs: 100,
+  batchSize,
   callbacks: {
     onEpochEnd: (epoch, log) => {
       console.log(`Epoch ${epoch}: loss = ${log.loss}`)
     },
     onTrainEnd: () => {
       console.log('End of training')
-      model.predict(tf.tensor3d([[ [1], [1], [1], [2], [1], [3], [1], [4], [1], [5] ]]))
-        .print()
+      const x = [[ [1], [1], [1], [2], [1], [3], [1], [4], [1], [5] ]]
+      model.save('file://mymodel')
+      model
+        .predict(tf.tensor3d(x))
+        .data()
+        .then(predictions => {
+          const chunk = 10
+          const temparray = []
+          for (let i = 0, j = predictions.length; i < j; i += chunk)
+              temparray.push(predictions.slice(i, i + chunk))
+          return temparray
+        }, [[]])
+        .then(predictions => predictions.map(arr => arr.indexOf(Math.max(...arr))))
+        .then(console.log)
     }
   }
 })
