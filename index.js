1efb757ef74c72ac8f0085945099ec4858fc6044
Kaelinator <kirkkael@gmail.com>
2018-08-04 00:10:11 -0500

    having shape issues

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ require('@tensorflow/tfjs-node')
 
 const rnn = tf.layers.rnn({
   cell: [
-    tf.layers.lstmCell({units: 5}),
-    tf.layers.lstmCell({units: 12}),
-    tf.layers.lstmCell({units: 1}),
+    tf.layers.lstmCell({units: 50})
   ],
-  returnSequences: true
+  returnSequences: true,
+  inputShape: [10, 1] // [timesteps, feature]
 })
 
 const model = tf.sequential({
   layers: [
-    tf.layers.dense({ units: 50, inputShape: [3, 10]}),
-    rnn
+    rnn,
+    tf.layers.dense({ units: 10 })
   ]
 })
 
@@ -23,22 +22,31 @@ model.compile({
   loss: 'meanSquaredError'
 })
 
-model.predict(tf.randomNormal([5, 3, 10])).print()
+/* [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, ... ] */
+const data = Int32Array.from(Array(1000)
+  .fill(0)
+  .reduce((arr, x, i) => arr.concat((i).toString().split('')), []), x => +x)
 
-// const xs = tf.randomNormal([100, 2])
-// const ys = tf.randomNormal([100, 1])
+const xs = tf.tensor1d(data).reshape([289, 10, 1])
+const ys = tf.tensor1d(Array.from(data).map(x => { 
+  const arr = Array(10).fill(0)
+  arr[x] = 1
+  return Int32Array.from(arr)
+})).reshape([289, 10, 10])
+console.log(xs.shape)
+console.log(ys.shape)
 
-// xs.print()
-// ys.print()
-
-// model.fit(xs, ys, {
-//   epochs: 100,
-//   callbacks: {
-//     onEpochEnd: (epoch, log) => {
-//       console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-//     },
-//     onTrainEnd: (x, y, z) => {
-//       console.log('x', x, 'y', y, 'z', z)
-//     }
-//   }
-// })
+model.fit(xs, ys, {
+  epochs: 1,
+  batchSize: 289,
+  callbacks: {
+    onEpochEnd: (epoch, log) => {
+      console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+    },
+    onTrainEnd: () => {
+      console.log('End of training')
+      model.predict(tf.tensor3d([[ [1], [1], [1], [2], [1], [3], [1], [4], [1], [5] ]]))
+        .print()
+    }
+  }
+})
