b1a6464e494423c01c0ba142821a145f55928618
Kaelinator <kirkkael@gmail.com>
2018-08-04 03:28:41 -0500

    saving and predicting

--- train.js
+++ train.js
@@ -3,9 +3,9 @@ require('@tensorflow/tfjs-node')
 
 const model = tf.sequential({
   layers: [
-    tf.layers.dense({ units: 16, activation: 'linear', inputShape: [10, 1] }),
-    tf.layers.lstm({ units: 128, returnSequences: true, recurrentActivation: 'softplus' }),
-    tf.layers.dense({ units: 10, activation: 'sigmoid' })
+    tf.layers.dense({ units: 10, inputShape: [10, 1] }),
+    tf.layers.lstm({ units: 512, returnSequences: true, recurrentActivation: 'softplus' }),
+    tf.layers.dense({ units: 10 })
   ]
 })
 
@@ -26,7 +26,7 @@ const xs = tf.tensor1d(data, 'int32').reshape([batchSize, 10, 1])
 const ys = tf.oneHot(tf.tensor1d([...data.slice(1), 0], 'int32'), 10).reshape([batchSize, 10, 10]).toFloat()
 
 model.fit(xs, ys, {
-  epochs: 100,
+  epochs: 3000,
   batchSize,
   callbacks: {
     onEpochEnd: (epoch, log) => {
@@ -34,20 +34,7 @@ model.fit(xs, ys, {
     },
     onTrainEnd: () => {
       console.log('End of training')
-      const x = [[ [1], [1], [1], [2], [1], [3], [1], [4], [1], [5] ]]
-      model.save('file://mymodel')
-      model
-        .predict(tf.tensor3d(x))
-        .data()
-        .then(predictions => {
-          const chunk = 10
-          const temparray = []
-          for (let i = 0, j = predictions.length; i < j; i += chunk)
-              temparray.push(predictions.slice(i, i + chunk))
-          return temparray
-        }, [[]])
-        .then(predictions => predictions.map(arr => arr.indexOf(Math.max(...arr))))
-        .then(console.log)
+      model.save('file://build/mymodel')
     }
   }
 })
